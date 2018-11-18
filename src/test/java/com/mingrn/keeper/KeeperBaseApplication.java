58aad3573ad0eeb6dcedfd7496e256740c284e9f
MinGRn <MinGRn97@gmail.com>
2018-11-18 18:31:07 +0800

    新增: keeper-parent 依赖;

---
+++ src/test/java/com/mingrn/keeper/KeeperBaseApplication.java
@@ -0,0 +1,12 @@
+package com.mingrn.keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeperBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeperBaseApplication.class, args);
+	}
+}
